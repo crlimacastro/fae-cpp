4b68d43fa7fae725184250c9d69090eed5d23401
crlimacastro <crlimacastro@users.noreply.github.com>
2024-06-06 03:44:18 +0000

    deploy: f02ba57756d0491adaba8893fec386e227f72117

---
+++ CMakeFiles/example_application.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_application.dir/example_application/example_application.cpp.o"
+  "CMakeFiles/example_application.dir/example_application/example_application.cpp.o.d"
+  "index.html"
+  "index.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
